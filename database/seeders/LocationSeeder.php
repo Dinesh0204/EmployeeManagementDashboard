ecb6b2e744c99d85fba6dbd56e526038fe26755c
Dinesh0204 <72650101+Dinesh0204@users.noreply.github.com>
2025-04-27 13:19:30 +0530

    Add latitude longitude in locations table and update seeder

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -13,11 +13,19 @@ class LocationSeeder extends Seeder
      */
     public function run(): void
     {
-        $locationData = ['Mumbai', 'Pune', 'Nashik', 'Hyderabad', 'Bengaluru'];
+        $locationData = [
+            ['name' => 'Mumbai', 'latitude' => 19.0760, 'longitude' => 72.8777],
+            ['name' => 'Pune', 'latitude' => 18.5204, 'longitude' => 73.8567],
+            ['name' => 'Nashik', 'latitude' => 19.9975, 'longitude' => 73.7898],
+            ['name' => 'Hyderabad', 'latitude' => 17.3850, 'longitude' => 78.4867]
+        ];
+
 
         $locations = array_map(function ($location) {
             return [
-                'name' => $location,
+                'name' => $location['name'],
+                'latitude' => $location['latitude'],
+                'longitude' => $location['longitude'],
                 'created_at' => now(),
                 'updated_at' => now()
             ];
